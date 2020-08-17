29aa38969011be22bd65c28de07a9b61f22d92fb
Marina Sergeyeva <morrrinad@gmail.com>
2020-08-17 17:09:33 +0300

    Updates

---
+++ precache-manifest.9129c3b691d8cdd458abdbdcd26c2a7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad0d939e5dd2bf9c4afd9bd8a8a4b16",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "15717b2c7636aff9ffa9",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.18288e56.chunk.css"
+  },
+  {
+    "revision": "fdf1e5b888bba35f9ad2",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.9505f6a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.9505f6a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15717b2c7636aff9ffa9",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.55715bdf.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
